c8a648aea60bd51e8dd96ecdd70c929ac01b5801
mechamsamuel <mechamsamuel@gmail.com>
2015-03-05 13:59:19 -0700

    Employer Competency #3 write simple unit test

---
+++ nodeAssignement/math.js
@@ -0,0 +1,13 @@
+//This function should add the arguments together
+exports.adder = function (num1,num2) {
+    var sum = num1 + num2;
+
+    return sum;
+}
+
+//This function should multiply the arguments together
+exports.multiplyer = function (num1,num2) {
+    var product = num1 * num2;
+
+    return product;
+}
---
+++ nodeAssignement/test.js
@@ -0,0 +1,24 @@
+var expect = require('expect');
+var math = require('./math.js');
+
+describe('adder', function () {
+    it('should add two arguments', function () {
+        expect(math.adder(2, 5)).toEqual(7);
+    });
+
+    it('should be a number', function () {
+        expect(math.adder(2, 5)).toBeA('number');
+    });
+
+});
+
+describe('multiplyer', function () {
+    it('should add two arguments', function () {
+        expect(math.multiplyer(2, 5)).toEqual(10);
+    });
+
+    it('should be a number', function () {
+        expect(math.adder(2, 5)).toBeA('number');
+    });
+
+});
